03048b645671b268a39f76074f0da0427c610d19
muldermind <jm@muldermind.com>
2025-05-31 15:45:27 +0200

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,5 @@
 // app/admin/layout.tsx
-import "../globals.css";
+import '../../styles/globals.css';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
